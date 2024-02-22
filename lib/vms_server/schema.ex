2a99a8a3118b6899452c5b3122481b3c1a6a7d93
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-02-22 16:44:31 -0300

    add macro to suport binary_id like id

---
+++ lib/vms_server/schema.ex
@@ -0,0 +1,13 @@
+defmodule VmsServer.Schema do
+  @moduledoc """
+
+  """
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts [type: :utc_datetime]
+    end
+  end
+end
