cd49973cb88d5f06a49acc3348b6d3db4eadf6b4
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-03-14 16:24:31 -0300

    create router to update sheet

--- lib/vms_server_web/controllers/sheet_controller.ex
+++ lib/vms_server_web/controllers/sheet_controller.ex
@@ -2,7 +2,7 @@ defmodule VmsServerWeb.SheetController do
   use VmsServerWeb, :controller
 
   action_fallback VmsServerWeb.FallbackController
-  plug VmsServer.Plugs.AtomizeParams when action in [:create]
+  plug VmsServer.Plugs.AtomizeParams when action in [:create, :update]
 
   def get_characteristics_fields(conn, %{"race_id" => race_id}) do
     case VmsServer.Sheet.get_characteristics_fields(race_id) do
@@ -20,9 +20,9 @@ defmodule VmsServerWeb.SheetController do
 
   def update(%{assigns: %{atomized_params: %{id: character_id} = params}} = conn, _params) do
     with {:ok, character} <- VmsServer.Sheet.get_character_by_id(character_id),
-         {:ok, character_updated} <- VmsServer.Sheet.update_character(character, params) do
-      character_updated
-      |> handle_response(conn, :character_fields, :created)
+         {:ok, _character_updated} <-
+           VmsServer.Sheet.update_character(character, params) do
+      handle_response(conn, :no_content)
     end
   end
 
@@ -31,4 +31,10 @@ defmodule VmsServerWeb.SheetController do
       conn
       |> put_status(status)
       |> render(view, fields: response)
+
+  defp handle_response(conn, status),
+    do:
+      conn
+      |> resp(status, "")
+      |> send_resp()
 end
