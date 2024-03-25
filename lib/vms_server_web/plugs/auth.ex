13e9938263aa385284f9e302359743f3bd601643
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-03-25 18:57:47 -0300

    refactor auth plug

--- lib/vms_server_web/plugs/auth.ex
+++ lib/vms_server_web/plugs/auth.ex
@@ -4,16 +4,26 @@ defmodule VmsServerWeb.Plugs.Auth do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
-         {:ok, data} <- VmsServerWeb.Token.verify(token) do
-      assign(conn, :user_id, data)
-    else
-      _error ->
-        conn
-        |> put_status(:unauthorized)
-        |> put_view(json: VmsServerWeb.ErrorJSON)
-        |> render(:error, %{error: :unauthorized})
-        |> halt()
+    case get_req_header(conn, "authorization") do
+      ["Bearer " <> token] ->
+        case VmsServerWeb.Token.verify(token) do
+          {:ok, data} ->
+            assign(conn, :user_id, data)
+
+          _error ->
+            unauthorized_response(conn)
+        end
+
+      _ ->
+        unauthorized_response(conn)
     end
+  end
+
+  defp unauthorized_response(conn) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(VmsServerWeb.ErrorJSON)
+    |> render(:error, %{error: :unauthorized})
+    |> halt()
   end
 end
